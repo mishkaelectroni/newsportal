b5b214a6bf9b3a0d89bdadc66c1b8fc01e157e71
mishkaelectroni <mishkaelectroni@gmail.com>
2017-09-05 16:25:35 +0300

    050917 demo newsportal React CRA
    050917 demo newsportal React CRA
    homepage

--- src/life.js
+++ src/life.js
@@ -5,7 +5,37 @@ import './life.css';
 class Life extends Component {
 	render() {
 		return (
-			<div className="life"><p>life</p></div>
+			<div className="life">
+				<div className="lifemain">
+					<h4><Link to="/life">LIFE</Link></h4>
+					<div className="lifecontent">
+						<div className="lifeone">
+							<Link to="/life"><img src={require('./img/life1.jpg')} alt="fotolife1" /> </Link>
+								
+						</div>
+						<div className="lifetwo">
+							<div className="lifetwoone">
+								<Link to="/life"><p>Prince Charles: rising intolerance risks repeat of horrors of past</p></Link>
+							</div>
+							<div className="lifetwotwo">
+								<Link to="/life"><p>Prince Charles: rising intolerance risks repeat of horrors of past</p></Link>
+							</div>
+						</div>
+						<div className="lifethree">
+							<Link to="/life"><img src={require('./img/life2.jpg')} alt="fotolife2" /> </Link>
+								
+						</div>
+						<div className="lifefour">
+							<div className="lifefourone">
+								<Link to="/life"><p>Prince Charles: rising intolerance risks repeat of horrors of past</p></Link>
+							</div>
+							<div className="lifefourtwo">
+								<Link to="/life"><p>Prince Charles: rising intolerance risks repeat of horrors of past</p></Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
--- src/people.js
+++ src/people.js
@@ -5,7 +5,29 @@ import './people.css';
 class People extends Component {
 	render() {
 		return (
-			<div className="people"><p>people</p></div>
+			<div className="people">
+				<div className="peoplemain">
+					<h4><Link to="/people">PEOPLE</Link></h4>
+					<div className="peoplecontent">
+						<div className="peopleone">
+							<Link to="/people"><img src={require('./img/people1.jpg')} alt="fotoepople1" /> </Link>
+								<p><Link to="/people">Franca Sozzani</Link>, editor in chief of Vouge Italia, dies aged 66</p>
+						</div>
+						<div className="peopletwo">
+							<Link to="/people"><img src={require('./img/people2.jpg')} alt="fotoepople1" /> </Link>
+								<p><Link to="/people">Make Will Smith great again</Link>, Charm, comedy, hit film after hit film</p>
+						</div>
+						<div className="peoplethree">
+							<Link to="/people"><img src={require('./img/people3.jpg')} alt="fotoepople1" /> </Link>
+								<p><Link to="/people">Gold,frankincense and Clarkson's Merc:</Link>the Sun's secret nativity play</p>
+						</div>
+						<div className="peoplefour">
+							<Link to="/people"><img src={require('./img/people4.jpg')} alt="fotoepople1" /> </Link>
+								<p><Link to="/people">Emma Stone:</Link> directorsgave my improv jokes to male co-stars</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
