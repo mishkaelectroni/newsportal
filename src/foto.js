c496bd15ca6d93411fe535d125d95197532f235c
mishkaelectroni <mishkaelectroni@gmail.com>
2017-09-06 15:56:17 +0300

    060917 demo nesportal home page CRA
    060917 demo nesportal home page CRA

--- src/foto.js
+++ src/foto.js
@@ -5,7 +5,26 @@ import './foto.css';
 class Foto extends Component {
 	render() {
 		return (
-			<div className="foto"><p>Foto</p></div>
+			<div className="foto">
+				<div className="fotomain">
+					<h4><Link to="/foto">FOTO OF THE DAY</Link></h4>
+					<div className="fotocontent">
+						<ul>
+							<li>
+								<Link to="/foto"><img src={require('./img/foto1.jpg')} alt="fotofoto1" /> </Link>
+							</li>
+							<li>
+								<h3>Your most underwhelming Christmas photos</h3>
+								<p>From Santa in Greggs to a Croydon of CHristmas past, our From Santa in Greggs to a Croydon of CHristmas past, our</p>
+								<p>It's almost here</p>
+								<p>From Santa in Greggs to a Croydon of CHristmas past, our From Santa in Greggs to a Croydon of CHristmas past, our</p>
+
+								<button>VIEW ALL FOTO'S &gt;</button>
+							</li>						
+						</ul>						
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
--- src/video.js
+++ src/video.js
@@ -5,7 +5,51 @@ import './video.css';
 class Video extends Component {
 	render() {
 		return (
-			<div className="video"><p>video</p></div>
+			<div className="video">
+				<div className="videomain">
+					<h4><Link to="/people">VIDEO</Link></h4>
+					<div className="videocontent">
+						<ul >
+							<li>
+								<Link to="/video"><img src={require('./img/video1.png')} alt="fotovideo1" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+							<li>
+								<Link to="/video"><img src={require('./img/video2.png')} alt="fotovideo2" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+							<li>
+								<Link to="/video"><img src={require('./img/video3.png')} alt="fotovideo3" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+							<li>
+								<Link to="/video"><img src={require('./img/video4.png')} alt="fotovideo4" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+						</ul>
+
+						<ul>
+							<li>
+								<Link to="/video"><img src={require('./img/video1.png')} alt="fotovideo1" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+							<li>
+								<Link to="/video"><img src={require('./img/video2.png')} alt="fotovideo2" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+							<li>
+								<Link to="/video"><img src={require('./img/video3.png')} alt="fotovideo3" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+							<li>
+								<Link to="/video"><img src={require('./img/video4.png')} alt="fotovideo4" /> </Link>
+								<p>Qui dolorem eum fugiat, quo voluptas nulaa pariatur</p>
+							</li>
+						</ul>
+						
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
